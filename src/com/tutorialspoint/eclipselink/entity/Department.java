ca97c1c7ac5ad3e87b3ae8b9bd448f533d917794
Kit Lawes <kitlawes@hotmail.com>
2017-08-16 15:57:07 +0100

    Update Department, update Employee, update persistence.xml, and add OneToMany to demonstrate the use of a OneToMany relation

--- src/com/tutorialspoint/eclipselink/entity/Department.java
+++ src/com/tutorialspoint/eclipselink/entity/Department.java
@@ -1,34 +1,45 @@
 package com.tutorialspoint.eclipselink.entity;
 
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.OneToMany;
 
-@Table
 @Entity
 public class Department {
 
-   @Id 
-   @GeneratedValue( strategy=GenerationType.AUTO )
+    @Id 
+    @GeneratedValue( strategy=GenerationType.AUTO )
+    
+    private int id;
+    private String name;
+    
+    @OneToMany( targetEntity=Employee.class )
+    private List employeelist;
 
-   private int id;
-   private String name;
+    public int getId() {
+    	return id;
+    }
+    
+    public void setId(int id) {
+    	this.id = id;
+    }
+    
+    public String getName( ) {
+    	return name;
+    }
+    
+    public void setName( String deptName ) {
+    	this.name = deptName;
+    }
 
-   public int getId() {
-      return id;
-   }
+    public List getEmployeelist() {
+      return employeelist;
+    }
 
-   public void setId(int id) {
-      this.id = id;
-   }
-
-   public String getName( ){
-      return name;
-   }
-
-   public void setName( String deptName ){
-      this.name = deptName;
+   public void setEmployeelist(List employeelist) {
+      this.employeelist = employeelist;
    }
 }
--- src/com/tutorialspoint/eclipselink/service/OneToMany.java
+++ src/com/tutorialspoint/eclipselink/service/OneToMany.java
@@ -1,5 +1,8 @@
 package com.tutorialspoint.eclipselink.service;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,45 +10,49 @@ import javax.persistence.Persistence;
 import com.tutorialspoint.eclipselink.entity.Department;
 import com.tutorialspoint.eclipselink.entity.Employee;
 
-public class ManyToOne {
-   public static void main( String[ ] args ) {
+public class OneToMany {
+   public static void main(String[] args) {
    
    EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Eclipselink_JPA" );
    EntityManager entitymanager = emfactory.createEntityManager( );
    entitymanager.getTransaction( ).begin( );
-
-   //Create Department Entity
-   Department department = new Department();
-   department.setName("Development");
-   
-   //Store Department
-   entitymanager.persist(department);
 
    //Create Employee1 Entity
    Employee employee1 = new Employee();
    employee1.setEname("Satish");
    employee1.setSalary(45000.0);
    employee1.setDeg("Technical Writer");
-//   employee1.setDepartment(department);
 
    //Create Employee2 Entity
    Employee employee2 = new Employee();
    employee2.setEname("Krishna");
    employee2.setSalary(45000.0);
    employee2.setDeg("Technical Writer");
-//   employee2.setDepartment(department);
 
    //Create Employee3 Entity
    Employee employee3 = new Employee();
    employee3.setEname("Masthanvali");
    employee3.setSalary(50000.0);
    employee3.setDeg("Technical Writer");
-//   employee3.setDepartment(department);
 
-   //Store Employees
+   //Store Employee
    entitymanager.persist(employee1);
    entitymanager.persist(employee2);
    entitymanager.persist(employee3);
+
+   //Create Employeelist
+   List<Employee> emplist = new ArrayList();
+   emplist.add(employee1);
+   emplist.add(employee2);
+   emplist.add(employee3);
+
+   //Create Department Entity
+   Department department = new Department();
+   department.setName("Development");
+   department.setEmployeelist(emplist);
+
+   //Store Department
+   entitymanager.persist(department);
 
    entitymanager.getTransaction().commit();
    entitymanager.close();
